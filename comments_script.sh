d52831ad1cc132b1d026c6ea6de1aa0863ac2285
Yazan Agha-Schrader <astronautist@icloud.com>
2023-06-11 22:02:56 +0200

    add script

---
+++ comments_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Stelle sicher, dass ein Argument angegeben wurde
+if [ $# -eq 0 ]; then
+    echo "Bitte gib den Dateinamen der Markdown-Datei an, z.B. ./comment_script.sh Fahrrad.md"
+    exit 1
+fi
+
+# Setze Variablen
+MD_FILE=$1
+IFRAME_URL="https://your-netlify-domain.com"
+GIT_REPO_PATH="~/obsidian/comments/"
+HTML_TEMPLATE_PATH="~/obsidian/comments/index.html"
+HTML_DESTINATION_PATH="$GIT_REPO_PATH/$(basename "$MD_FILE" .md).html"
+
+# Kopiere die HTML-Vorlage und benenne sie mit dem Dateinamen um
+cp "$HTML_TEMPLATE_PATH" "$HTML_DESTINATION_PATH"
+
+# Füge den iframe zur Markdown-Datei hinzu
+echo "<iframe src=\"$IFRAME_URL/$(basename "$HTML_DESTINATION_PATH")\" width=\"100%\" height=\"400\" frameborder=\"0\"></iframe>" >> "$MD_FILE"
+
+# Navigiere zum Git-Repo, committe und pushe die Änderungen
+cd "$GIT_REPO_PATH"
+git add "$(basename "$HTML_DESTINATION_PATH")"
+git commit -m "Hinzufügen von $(basename "$HTML_DESTINATION_PATH") für Kommentarbereich"
+git push
