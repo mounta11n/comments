01b33d29ff7a9a83b23f6db943b89afd183d100b
Yazan Agha-Schrader <astronautist@icloud.com>
2023-06-11 22:33:25 +0200

    set netlify endpoint

--- comments_script.sh
+++ comments_script.sh
@@ -8,15 +8,17 @@ fi
 
 # Setze Variablen
 MD_FILE=$1
-IFRAME_URL="https://your-netlify-domain.com"
-GIT_REPO_PATH="~/obsidian/comments/"
-HTML_TEMPLATE_PATH="~/obsidian/comments/index.html"
+IFRAME_URL="https://mountaiin-comments.netlify.app/"
+GIT_REPO_PATH="$HOME/obsidian/comments"
+HTML_TEMPLATE_PATH="$HOME/obsidian/comments/index.html"
 HTML_DESTINATION_PATH="$GIT_REPO_PATH/$(basename "$MD_FILE" .md).html"
 
 # Kopiere die HTML-Vorlage und benenne sie mit dem Dateinamen um
 cp "$HTML_TEMPLATE_PATH" "$HTML_DESTINATION_PATH"
 
 # Füge den iframe zur Markdown-Datei hinzu
+echo >> "$MD_FILE"
+echo >> "$MD_FILE"
 echo "<iframe src=\"$IFRAME_URL/$(basename "$HTML_DESTINATION_PATH")\" width=\"100%\" height=\"400\" frameborder=\"0\"></iframe>" >> "$MD_FILE"
 
 # Navigiere zum Git-Repo, committe und pushe die Änderungen
