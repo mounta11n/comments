1e1c2de85c76e8a3a474ec8a9c38f996d2d8ec25
Yazan Agha-Schrader <astronautist@icloud.com>
2023-06-14 01:07:54 +0200

    Hinzufügen von magic diffusion.html für Kommentarbereich

--- comments_script.sh
+++ comments_script.sh
@@ -17,8 +17,11 @@ HTML_DESTINATION_PATH="$GIT_REPO_PATH/$(basename "$MD_FILE" .md).html"
 cp "$HTML_TEMPLATE_PATH" "$HTML_DESTINATION_PATH"
 
 # Füge den iframe zur Markdown-Datei hinzu
+echo "<br>" >> "$MD_FILE"
+echo "<br>" >> "$MD_FILE"
 echo >> "$MD_FILE"
-echo >> "$MD_FILE"
+echo "> [!info]" >> "$MD_FILE"
+echo '> <a href="https://648601e72c003b3086bef838--wondrous-malabi-faf689.netlify.app/login.html" style="color: #20deeb; font-weight: bold;">Click here to sign in and comment. Currently, unfortunately, logging in and commenting must take place in a new tab or window.</a>' >> "$MD_FILE"
 echo "<iframe src=\"$IFRAME_URL/$(basename "$HTML_DESTINATION_PATH")\" width=\"100%\" height=\"400\" frameborder=\"0\"></iframe>" >> "$MD_FILE"
 
 # Navigiere zum Git-Repo, committe und pushe die Änderungen
